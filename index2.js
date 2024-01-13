4864d2e24db1eb64a1fc0e53c2d3bb527d2856a8
Hiruna <116061206+hirunch@users.noreply.github.com>
2024-01-13 19:55:05 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,12 @@
 
 document.getElementById("mon").onclick = function(){
 
+  document.getElementById("tueR").textContent = "";
+  document.getElementById("wenR").textContent = "";
+  document.getElementById("thrR").textContent = "";
+  document.getElementById("friR").textContent = "";
+  document.getElementById("satR").textContent = "";
+  document.getElementById("sunR").textContent = '';
   document.getElementById("monR").textContent = ' 07:30 - 09:00';
 }
 
@@ -8,35 +14,68 @@ document.getElementById("mon").onclick = function(){
 document.getElementById("tue").onclick = function(){
 
   document.getElementById("monR").textContent = "";
+  document.getElementById("wenR").textContent = "";
+  document.getElementById("thrR").textContent = "";
+  document.getElementById("friR").textContent = "";
+  document.getElementById("satR").textContent = "";
+  document.getElementById("sunR").textContent = "";
   document.getElementById("tueR").textContent = ' 03:00 - 04:30';
 }
 
 document.getElementById("wen").onclick = function(){
 
+  document.getElementById("monR").textContent = "";
   document.getElementById("tueR").textContent = "";
+  document.getElementById("thrR").textContent = "";
+  document.getElementById("friR").textContent = "";
+  document.getElementById("satR").textContent = "";
+  document.getElementById("sunR").textContent = "";
   document.getElementById("wenR").textContent = ' 12:00 - 01:30';
 }
 
 document.getElementById("thr").onclick = function(){
 
+  document.getElementById("monR").textContent = "";
+  document.getElementById("tueR").textContent = "";
   document.getElementById("wenR").textContent = "";
+  document.getElementById("friR").textContent = "";
+  document.getElementById("satR").textContent = "";
+  document.getElementById("sunR").textContent = "";
   document.getElementById("thrR").textContent = ' 01:30 - 03:00';
 }
 
 document.getElementById("fri").onclick = function(){
 
+
+  document.getElementById("monR").textContent = "";
+  document.getElementById("tueR").textContent = "";
+  document.getElementById("wenR").textContent = "";
   document.getElementById("thrR").textContent = "";
+  document.getElementById("satR").textContent = "";
+  document.getElementById("sunR").textContent = "";
   document.getElementById("friR").textContent = ' 10:30 - 12:00';
 }
 
 document.getElementById("sat").onclick = function(){
 
+  
+  document.getElementById("monR").textContent = "";
+  document.getElementById("tueR").textContent = "";
+  document.getElementById("wenR").textContent = "";
+  document.getElementById("thrR").textContent = "";
   document.getElementById("friR").textContent = "";
+  document.getElementById("sunR").textContent = "";
   document.getElementById("satR").textContent = ' 09:00 - 10:30';
 }
 
 document.getElementById("sun").onclick = function(){
 
+  
+  document.getElementById("monR").textContent = "";
+  document.getElementById("tueR").textContent = "";
+  document.getElementById("wenR").textContent = "";
+  document.getElementById("thrR").textContent = "";
+  document.getElementById("friR").textContent = "";
   document.getElementById("satR").textContent = "";
   document.getElementById("sunR").textContent = ' 04:30 - 06:00';
 }
@@ -44,6 +83,12 @@ document.getElementById("sun").onclick = function(){
 
 document.getElementById("clr").onclick = function(){
 
+  document.getElementById("monR").textContent = "";
+  document.getElementById("tueR").textContent = "";
+  document.getElementById("wenR").textContent = "";
+  document.getElementById("thrR").textContent = "";
+  document.getElementById("friR").textContent = "";
+  document.getElementById("satR").textContent = "";
   document.getElementById("sunR").textContent = '';
   
 }
